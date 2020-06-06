56fe5473c51841d7a7857f0c7db1cc45fe378ef7
Dominik Tarnowski <dom@tdom.dev>
2020-06-06 22:48:13 +0100

    feat(FZF): add basic FZF Frontend

---
+++ src/interpreter/frontends/fzf.rs
@@ -0,0 +1,75 @@
+use super::Frontend;
+use crate::ast::{Snippet, Token};
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
+
+const DEFAULT_ARGS: &'static [&str] = &[
+    "--preview-window",
+    "bottom:1",
+    "--bind",
+    "ctrl-space:print-query",
+    "-e",
+    "-i",
+];
+
+pub struct FZF {}
+
+impl Frontend for FZF {
+    fn select_snippet<'a>(&self, snippets: &'a Vec<Snippet>) -> &'a Snippet {
+        let mut p = Command::new("fzf")
+            .args(DEFAULT_ARGS.iter().chain(&[
+                "--preview",
+                "echo {} | cut -d'\t' -f2", // second column is the preview
+                "--tabstop",
+                &FZF::get_tabsize(snippets).to_string(),
+            ]))
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+        let stdin = p.stdin.as_mut().unwrap();
+
+        let mut displayed = String::from("");
+        for snippet in snippets {
+            // TODO: actually get the 1st executable value
+            let command = format!("{}", snippet);
+            displayed.push_str(&format!("{}\t{}\n", &snippet.description, command));
+        }
+        stdin.write(displayed.as_bytes()).unwrap();
+
+        let o = p.wait_with_output().unwrap();
+        let output_str = std::str::from_utf8(&o.stdout).unwrap();
+        let columns_output: Vec<&str> = output_str.split("\t").collect();
+        let chosen_action = columns_output.get(0).unwrap();
+
+        for snippet in snippets {
+            if &snippet.description == chosen_action {
+                return snippet;
+            }
+        }
+
+        panic!()
+    }
+    fn select_set_value(&self, tokens: &Vec<Token>, set: &Vec<String>) -> String {
+        String::from("fak")
+    }
+}
+
+impl FZF {
+    pub fn new() -> Self {
+        FZF {}
+    }
+
+    // get the longest description and add padding to it to get the tab size
+    fn get_tabsize(snippets: &Vec<Snippet>) -> usize {
+        let mut size = 4;
+        for s in snippets {
+            if s.description.len() > size {
+                size = s.description.len();
+            }
+        }
+        size + 2 // we need at least 1 padding, otherwise widest column will no be displayed properly
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+mod ast;
+mod interpreter;
+
+use ast::{Scope, Set, SetValue, Snippet, SnippetValue, Token};
+use interpreter::frontends::FZF;
+use interpreter::Interpreter;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: if --validate flag is present:
+    // run the SemanticAnalyzer to check for any errors
+    // panic on error
+    let scope = Scope {
+        sets: vec![
+            Set {
+                identifier: String::from("user"),
+                values: vec![SetValue::Text(String::from("root"))],
+            },
+            Set {
+                identifier: String::from("host"),
+                values: vec![SetValue::Text(String::from("localhost"))],
+            },
+        ],
+        snippets: vec![
+            Snippet {
+                description: String::from("remote connect to user"),
+                values: vec![SnippetValue {
+                    decorators: vec![],
+                    tokens: vec![
+                        Token::Text(String::from("ssh ")),
+                        Token::Reference(String::from("user")),
+                        Token::Text(String::from("@soton.cloud")),
+                    ],
+                }],
+            },
+            Snippet {
+                description: String::from("ssh and run command"),
+                values: vec![SnippetValue {
+                    decorators: vec![],
+                    tokens: vec![
+                        Token::Text(String::from("ssh ")),
+                        Token::Reference(String::from("user")),
+                        Token::Text(String::from("@")),
+                        Token::Reference(String::from("host")),
+                        Token::Text(String::from(" ")),
+                        Token::Reference(String::from("command")),
+                    ],
+                }],
+            },
+        ],
+    };
+    let program = Interpreter::new(&scope, FZF::new());
+    program.run();
 }
