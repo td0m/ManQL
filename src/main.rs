cdb9e4876fd48d96e0e9cf04be83eda1d558c092
Dominik Tarnowski <dom@tdom.dev>
2020-06-07 12:31:44 +0100

    fix: add valid sample ast structure

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,24 @@ fn main() {
     // TODO: if --validate flag is present:
     // run the SemanticAnalyzer to check for any errors
     // panic on error
+    // TODO: if fzf, run fzf frontend
+    // TODO: if search {{command}}, return TLDR style
     let scope = Scope {
         sets: vec![
             Set {
                 identifier: String::from("user"),
-                values: vec![SetValue::Text(String::from("root"))],
+                values: vec![
+                    SetValue::Code(String::from("grep wheel /etc/group | cut -d':' -f4")),
+                    SetValue::Text(String::from("root")),
+                ],
             },
             Set {
                 identifier: String::from("host"),
                 values: vec![SetValue::Text(String::from("localhost"))],
+            },
+            Set {
+                identifier: String::from("command"),
+                values: vec![SetValue::Text(String::from("ls -la"))],
             },
         ],
         snippets: vec![
@@ -28,7 +37,8 @@ fn main() {
                     tokens: vec![
                         Token::Text(String::from("ssh ")),
                         Token::Reference(String::from("user")),
-                        Token::Text(String::from("@soton.cloud")),
+                        Token::Text(String::from("@")),
+                        Token::Reference(String::from("host")),
                     ],
                 }],
             },
